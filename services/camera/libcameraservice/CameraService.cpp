305e8e9eb0efc9f341d4aa702bda7581c0c68e51
Ziauddin Sameer <ziauddinsameer@gmail.com>
2022-03-22 09:57:20 +0000

    Revert "Camera: Miscellaneous fixes in QDataCallback and binder death scenarios."
    This reverts commit 8ce965115927f9249f99fef6b835701541e0a3dd.

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -2711,8 +2711,7 @@ bool CameraService::evictClientIdByRemote(const wp<IBinder>& remote) {
                 ret = true;
             }
         }
-        //clear the evicted client list before acquring service lock again.
-        evicted.clear();
+
         // Reacquire mServiceLock
         mServiceLock.lock();
 
